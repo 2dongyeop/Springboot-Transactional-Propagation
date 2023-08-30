f8f4ff897ab2b0480345b16ad72535cdb7db494c
leedongyeop <ldy_1204@naver.com>
2023-08-30 22:31:32 +0900

    initial commit

---
+++ src/main/java/io/dongyeop/transactionalpropagation/TransactionalPropagationApplication.java
@@ -0,0 +1,13 @@
+package io.dongyeop.transactionalpropagation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionalPropagationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionalPropagationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/dongyeop/transactionalpropagation/TransactionalPropagationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dongyeop.transactionalpropagation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionalPropagationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
